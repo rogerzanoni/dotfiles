490903b6df04b5004ae116b09f3bc66db231fb46
Roger Zanoni <rogerzanoni@gmail.com>
2017-09-18 15:28:27 -0300

    UTF-8 everywhere

--- config/newemacs/settings/editor-settings.el
+++ config/newemacs/settings/editor-settings.el
@@ -32,7 +32,14 @@
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 
+(set-language-environment 'utf-8)
+(setq locale-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(unless (eq system-type 'windows-nt)
+  (set-selection-coding-system 'utf-8))
 (prefer-coding-system 'utf-8)
+
 (when (display-graphic-p)
   (setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING)))
 
