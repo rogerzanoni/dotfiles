7371582a97e067fae0b38cb660563948a6e42ab8
Roger Zanoni <rogerzanoni@gmail.com>
2018-04-05 16:39:24 +0200

    Add a command to display helm semantic or imenu

--- config/newemacs/settings/cedet-settings.el
+++ config/newemacs/settings/cedet-settings.el
@@ -9,7 +9,8 @@
 (defun rz/cedet-hook ()
   (evil-leader/set-key
     "j" 'semantic-ia-fast-jump
-    "cs" 'semantic-ia-show-summary))
+    "cs" 'semantic-ia-show-summary
+    "ci" 'helm-semantic-or-imenu))
 
 (add-hook 'c-mode-common-hook 'rz/cedet-hook)
 (add-hook 'c-mode-hook 'rz/cedet-hook)
