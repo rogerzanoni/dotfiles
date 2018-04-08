1109b19dfefaa8b4581be5dcc32c9d821e69e12b
Roger Zanoni <rogerzanoni@gmail.com>
2018-04-07 21:50:13 -0300

    Better helm defaults and keybindings

--- config/newemacs/settings/helm-settings.el
+++ config/newemacs/settings/helm-settings.el
@@ -1,9 +1,21 @@
 (use-package helm
   :config
-  (setq helm-split-window-default-side 'below)
+  (setq helm-split-window-in-side-p t
+        helm-move-to-line-cycle-in-source t
+        helm-buffer-skip-remote-checking t
+        helm-mode-fuzzy-match t
+        helm-buffers-fuzzy-matching t
+        helm-buffer-skip-remote-checking t
+        helm-M-x-fuzzy-match t
+        helm-imenu-fuzzy-match t
+        helm-lisp-fuzzy-completion t
+        helm-buffer-skip-remote-checking t
+        helm-locate-fuzzy-match t
+        )
   (helm-mode 1)
   :bind
-  (("M-x" . helm-M-x)))
+  (("M-x" . helm-M-x)
+   ("M-y" . helm-show-kill-ring)))
 
 (use-package helm-ls-git
   :config
